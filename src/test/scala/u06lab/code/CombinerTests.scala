6d304526bf947ba4cb6354985ea4760321c5e421
Manuel Andruccioli <manuel.andruccioli@studio.unibo.it>
2023-04-17 11:34:40 +0200

    init

--- src/test/scala/u06lab/code/CombinerTests.scala
+++ src/test/scala/u06lab/code/CombinerTests.scala
@@ -3,9 +3,9 @@ package u06lab.code
 import org.junit.Test
 import org.junit.Assert.*
 
-class CombinerTests {
-  @Test
-  def testFunctions() = {
+class CombinerTests:
+
+  @Test def testFunctions(): Unit =
     val f: Functions = FunctionsImpl
     assertEquals(60.1, f.sum(List(10.0, 20.0, 30.1)), 0.001) // 60.1
     println((0.0, f.sum(List()))) // 0.0
@@ -13,5 +13,3 @@ class CombinerTests {
     println(("", f.concat(Seq())))
     println((3, f.max(List(-10, 3, -5, 0))))
     println((Integer.MIN_VALUE, f.max(List())))
-  }
-}
