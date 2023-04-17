0af562ed12cc60e9349ad71ce071cba084430caa
Manuel Andruccioli <manuel.andruccioli@studio.unibo.it>
2023-04-17 21:30:43 +0200

    feat(2.1): improve not two consecutive parser

--- src/main/scala/u06lab/code/Parser.scala
+++ src/main/scala/u06lab/code/Parser.scala
@@ -27,10 +27,15 @@ trait NonEmpty[T] extends Parser[T]:
 class NonEmptyParser(chars: Set[Char]) extends BasicParser(chars) with NonEmpty[Char]
 
 trait NotTwoConsecutive[T] extends Parser[T]:
-  val todo = ???
-// ???
+  private[this] var notTwoConsecutive = true
+  private[this] var last: Option[T] = None
+  abstract override def parse(t: T): Boolean =
+    notTwoConsecutive = if last.contains(t) then false else notTwoConsecutive
+    last = Option(t)
+    super.parse(t)
+  abstract override def end: Boolean = notTwoConsecutive && super.end
 
-class NotTwoConsecutiveParser(chars: Set[Char]) extends BasicParser(chars) // with ????
+class NotTwoConsecutiveParser(chars: Set[Char]) extends BasicParser(chars) with NotTwoConsecutive[Char]
 
 @main def checkParsers(): Unit =
   def parser = new BasicParser(Set('a', 'b', 'c'))
