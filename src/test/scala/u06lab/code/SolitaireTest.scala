e3498f036cf3957869057e313f7a7bbe938b52c8
Manuel Andruccioli <manuel.andruccioli@studio.unibo.it>
2023-04-26 22:59:22 +0200

    refactor(3): move to single file

--- src/test/scala/u06lab/code/SolitaireTest.scala
+++ src/test/scala/u06lab/code/SolitaireTest.scala
@@ -3,12 +3,31 @@ package u06lab.code
 import org.junit.Test
 import org.junit.Assert.*
 
+enum Content:
+  case Empty    // X = Empty
+  case Number   // N = Number
+  case Safe     // S = Safe for N
+
+class Board:
+  import Content.*
+  def X(using builder: BoardBuilder): Board =
+    builder.add(Empty); this
+  def N(using builder: BoardBuilder): Board =
+    builder.add(Number); this
+  def S(using builder: BoardBuilder): Board =
+    builder.add(Safe); this
+
+class BoardBuilder(width: Int, height: Int):
+  private val position: Iterator[Position] =
+    (for i <- 0 until width; j <- 0 until height yield (i, j)).iterator
+  var solution: Map[Position, Content] = Map()
+  def add(content: Content): Unit = solution = solution + (position.next -> content)
+
 class SolitaireTest:
 
   @Test def isSafeTest =
-    given builder: BoardBuilder = BoardBuilder(5, 7)
     val center = (2, 3)
-    // A sort of DSL: X = Empty, N = Number, S = Safe for N
+    given builder: BoardBuilder = BoardBuilder(5, 7)
     Board()
       .X.S.X.X.X.S.X
       .X.X.X.X.X.X.X
